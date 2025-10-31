1153586d26fc4421d5d3f59cc503583760c0bdcb
Brylle <bryanpalay119@gmail.com>
2025-10-31 18:37:37 +0800

    middleware update to improve request handling

--- middleware.ts
+++ middleware.ts
@@ -1,20 +1,19 @@
 import { NextRequest, NextResponse } from "next/server";
-import { headers } from "next/headers";
-import { auth } from "@/lib/auth";
+import { getSessionCookie } from "better-auth/cookies";
 
 export async function middleware(request: NextRequest) {
-  const session = await auth.api.getSession({
-    headers: await headers(),
-  });
+	const sessionCookie = getSessionCookie(request);
 
-  if (!session) {
-    return NextResponse.redirect(new URL("/sign-in", request.url));
-  }
+    // THIS IS NOT SECURE!
+    // This is the recommended approach to optimistically redirect users
+    // We recommend handling auth checks in each page/route
+	if (!sessionCookie) {
+		return NextResponse.redirect(new URL("/sign-in", request.url));
+	}
 
-  return NextResponse.next();
+	return NextResponse.next();
 }
 
 export const config = {
-  runtime: "nodejs",
-  matcher: ["/dashboard"], // Apply middleware to specific routes
+	matcher: ["/dashboard"], // Specify the routes the middleware applies to
 };

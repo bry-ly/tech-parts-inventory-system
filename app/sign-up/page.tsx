94931b7b2674bf0c55f514d89d0c4393e1f7db54
Brylle <bryanpalay119@gmail.com>
2025-11-03 00:55:55 +0800

    auth

--- app/sign-up/page.tsx
+++ app/sign-up/page.tsx
@@ -1,6 +1,6 @@
-import { Cpu } from "lucide-react"
+import { Cpu } from "lucide-react";
 
-import { SignupForm } from "@/components/auth/signup-form"
+import { SignupForm } from "@/components/auth/signup-form";
 import Link from "next/link";
 import { auth } from "@/lib/auth/auth";
 import { headers } from "next/headers";
@@ -13,10 +13,15 @@ export default async function SignupPage() {
   }
 
   return (
-    <div className="bg-muted flex min-h-svh flex-col items-center justify-center gap-6 p-6 md:p-10">
+    <div className="bg-muted flex min-h-svh flex-col items-center justify-center gap-6 p-6 md:p-10 lg:h-screen sm:h-screen md:h-screen">
       <div className="flex w-full max-w-sm flex-col gap-6">
-        <Link href="/" className="flex items-center gap-2 self-center font-medium">
-          <Cpu className="size-4" />
+        <Link
+          href="/"
+          className="flex items-center gap-2 self-center font-medium"
+        >
+          <div className="bg-primary text-primary-foreground flex size-6 items-center justify-center rounded-md">
+            <Cpu className="size-4" />
+          </div>
           Tech Parts Inc.
         </Link>
         <SignupForm />

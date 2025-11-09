04f9d818203273f2df8667b737d3c40ad753dbaf
Brylle <bryanpalay119@gmail.com>
2025-11-09 21:47:49 +0800

    signup

--- app/sign-up/page.tsx
+++ app/sign-up/page.tsx
@@ -13,7 +13,7 @@ export default async function SignupPage() {
   }
 
   return (
-    <div className="bg-muted flex min-h-svh flex-col items-center justify-center gap-6 p-6 md:p-10 lg:h-screen sm:h-screen md:h-screen">
+    <div className="bg-muted flex min-h-svh flex-col items-center justify-center gap-6 p-6 md:p-10">
       <div className="flex w-full max-w-sm flex-col gap-6">
         <Link
           href="/"

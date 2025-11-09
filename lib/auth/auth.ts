fc712f795a56d9be3fd95e9a1648bd0cc968fca7
Brylle <bryanpalay119@gmail.com>
2025-11-10 01:53:08 +0800

    auth server

--- lib/auth/auth.ts
+++ lib/auth/auth.ts
@@ -13,6 +13,14 @@ export const auth = betterAuth({
   emailAndPassword: {
     enabled: true,
   },
+  socialProviders: {
+    google: {
+      prompt: "select_account",
+      accessType: "offline",
+      clientId: process.env.GOOGLE_CLIENT_ID as string,
+      clientSecret: process.env.GOOGLE_CLIENT_SECRET as string,
+    },
+  },
   plugins: [nextCookies()],
 });
 

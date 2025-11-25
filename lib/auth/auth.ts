bdd126128e70ab90bf4aad130cad985eadfeb3e4
Brylle <bryanpalay119@gmail.com>
2025-11-25 23:37:50 +0800

    refactor: Update authentication module to use centralized Prisma instance and streamline database integration for better maintainability.

--- lib/auth/auth.ts
+++ lib/auth/auth.ts
@@ -1,10 +1,8 @@
 import { betterAuth } from "better-auth";
 import { prismaAdapter } from "better-auth/adapters/prisma";
-import { PrismaClient } from "@prisma/client";
+import { prisma } from "@/lib/prisma/prisma";
 import { nextCookies } from "better-auth/next-js";
 import { headers } from "next/headers";
-
-const prisma = new PrismaClient();
 
 export const auth = betterAuth({
   database: prismaAdapter(prisma, {
